b06631227feec9a14685aa2f11b0686cadfc8394
winter-pitts <wmpitts@iu.edu>
2022-12-09 22:26:11 -0500

    Complete Exercise 6d

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,9 @@ extends KinematicBody
 
 onready var Camera = $Pivot/Camera
 
+onready var Explosion = load("res://Explosion/Explosion.tscn")
+onready var Explosions = get_node("/root/Game/Explosions")
+
 var gravity = -30
 var max_speed = 8
 var mouse_sensitivity = 0.002
@@ -35,5 +38,8 @@ func _physics_process(_delta):
 		$Pivot/Laser.show()
 		if $Pivot/RayCast.is_colliding():
 			var target = $Pivot/RayCast.get_collider()
+			var explosion = Explosion.instance()
+			Explosions.add_child(explosion)
+			explosion.global_transform.origin = $Pivot/RayCast.get_collision_point()
 			if target.is_in_group("target"):
 				target.die()
